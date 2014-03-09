7bb2f73eddbe4e5b5c2ae920bb3ad2a8a560bf3c
gautampk <gautampk@linux.com>
2014-03-09 10:11:10 +0000

    hi

--- src/com/stackunderflow/findit/Remember.java
+++ src/com/stackunderflow/findit/Remember.java
@@ -1,166 +1,63 @@
-package com.stackUnderflow.findit;
+package com.stackunderflow.findit;
 
 import java.io.File;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.FileObserver;
 import android.provider.MediaStore;
-import android.view.KeyEvent;
+import android.net.Uri;
+import android.speech.RecognizerIntent;
 import android.view.MotionEvent;
-
+import android.view.View;
+import java.sql.Timestamp;
+import com.google.android.glass.app.Card;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import com.google.android.glass.media.CameraManager;
-import com.google.android.glass.touchpad.Gesture;
-import com.google.android.glass.touchpad.GestureDetector;
 
 public class Remember extends Activity {
-	protected int keyCode;
-	private GestureDetector gesture = null;
-	protected static final int TAKE_PICTURE = 1;
-	private CameraView cameraView = null;
+    public String filepath = Environment.getExternalStorageDirectory()+"/Pictures/FindIt/";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		cameraView = new CameraView(this);
-		gesture = createGestureDetector(this);
-		// setContentView(card1.toView());
-		this.setContentView(cameraView);
-		File f = new File(Environment.DIRECTORY_PICTURES + "Pictures/FindIt");
-		if(!f.exists()){
-			f.mkdir();
-			f.mkdirs();
-		}
+        File f = new File(filepath);
+        if(!f.exists()){
+            f.mkdir();
+            f.mkdirs();
+        }
+
+        Card cam = new Card(this);
+        View camView = cam.toView();
+        camView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                takePicture();
+
+                return true;
+            }
+        });
+        setContentView(camView);
 	}
 
-	protected void onStop() {
-		super.onStop();
-		cameraView = null;
-		finish();
-	}
+    private static final int TAKE_PICTURE_REQUEST = 1;
 
-	protected void onResume() {
-		super.onResume();
-		if (cameraView != null) {
-			cameraView.releaseCamera();
-		}
-		this.setContentView(cameraView);
-	}
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, TAKE_PICTURE_REQUEST);
+    }
 
-	protected void onPause() {
-		super.onPause();
-		if (cameraView != null) {
-			cameraView.releaseCamera();
-		}
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK) {
+            String picturePath = data.getStringExtra(CameraManager.EXTRA_PICTURE_FILE_PATH);
+            ArrayList<String> voiceResults = getIntent().getExtras().getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
+        }
 
-	private GestureDetector createGestureDetector(Context context) {
-		GestureDetector gestureDetector = new GestureDetector(context);
-
-		gestureDetector.setBaseListener(new GestureDetector.BaseListener() {
-
-			@Override
-			public boolean onGesture(Gesture gesture) {
-				if (cameraView != null) {
-					if (gesture.equals(Gesture.TAP)) {
-						Intent intent = new Intent(
-								MediaStore.ACTION_IMAGE_CAPTURE);
-						if (intent != null) {
-							startActivityForResult(intent, TAKE_PICTURE);
-						}
-						return true;
-					}
-				}
-				return false;
-			}
-		});
-		return gestureDetector;
-
-	}
-
-	public boolean onGenericMotionEvent(MotionEvent event) {
-		if (gesture != null) {
-			return gesture.onMotionEvent(event);
-		}
-		return false;
-	}
-
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == TAKE_PICTURE && resultCode == RESULT_OK) {
-			String picturePath = data
-					.getStringExtra(CameraManager.EXTRA_PICTURE_FILE_PATH);
-			processPictureWhenReady(picturePath);
-		}
-		super.onActivityResult(requestCode, resultCode, data);
-	}
-
-	private void processPictureWhenReady(final String picturePath) {
-		final File pictureFile = new File(picturePath);
-		if (pictureFile.exists()) {
-
-		} else {
-			pictureFile.mkdir();
-			FileObserver observer = new FileObserver(pictureFile.getPath()) {
-				private boolean isFileWritten;
-
-				@Override
-				public void onEvent(int event, String path) {
-
-					if (!isFileWritten) {
-						File affectedFile = new File(pictureFile, path);
-						isFileWritten = (event == FileObserver.CLOSE_WRITE && affectedFile
-								.equals(pictureFile));
-						if (isFileWritten) {
-							stopWatching();
-							runOnUiThread(new Runnable() {
-								@Override
-								public void run() {
-									processPictureWhenReady(picturePath);
-								}
-							});
-						}
-					}
-				}
-			};
-			observer.startWatching();
-		}
-
-	}
-
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-		/*
-		 * if (true) { final String truth =
-		 * "OLLIE IS A PRO, AND FIXED THIS FOR YOU ;)"; }
-		 */
-
-		if (keyCode == KeyEvent.KEYCODE_CAMERA) {
-			return false;
-		} else {
-			return super.onKeyDown(keyCode, event);
-		}
-		// if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
-		//
-		// // Capture image, save to *filename (filename =
-		// // Calendar.getInstance().time() ?)
-		//
-		// /*
-		// * procImg.storeLocation(filename, this); //add EXIF geoloc data
-		// */
-		//
-		// Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		// if (intent != null) {
-		// startActivityForResult(intent, TAKE_PICTURE);
-		// intent = null;
-		// }
-		// return true;
-		// } else if (keyCode == KeyEvent.KEYCODE_BACK) {
-		// onStop();
-		// }
-
-	}
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
